c798ead6e5c965ce96f68341ed3e1fe0d7284c8f
alexiefalu11 <alexie_15@hotmail.com>
2017-03-28 12:48:21 -0400

    Add files via upload

---
+++ sllNode.java
@@ -0,0 +1,29 @@
+//package testingproject;
+
+public class sllNode{
+	public Actor actor; //insert last name first
+	public sllNode next;
+
+	//Constructors
+	public sllNode(){
+		this.actor = new Actor();
+		this.next = null;
+	}
+
+	public sllNode(Actor actor){
+		this.actor = actor;
+		this.next = null;
+	}
+
+	//Get methods
+	public Actor getActor(){return this.actor;}
+	public sllNode getNext(){return this.next;}
+
+	//Set methods
+	public void setActor(Actor actor){
+		this.actor = actor;
+	}
+	public void setNext(sllNode next){
+		this.next = next;
+	}
+}
